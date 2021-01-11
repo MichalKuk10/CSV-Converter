08013b18503e60186c857ed2fd3c00f94717cf5b
michal <kuk.michal3@gmail.com>
2021-01-11 15:14:02 +0100

    add new class to convert files

--- src/main/java/ConverterApplication.java
+++ src/main/java/ConverterApplication.java
@@ -1,9 +1,19 @@
+
+
 public class ConverterApplication {
     public static void main(String[] args) {
+
         if (args.length == 0) {
             System.out.println("No input file defined");
+        }else if (args[0].equals("xml")){
+            System.out.println("dotarlo");
+        }else if(args[0].equals("json")){
+            System.out.println("dotarlo do json");
+        }else if(args[0].equals("")){
+            System.out.println("dotarlo do pustego");
         }
 
-
     }
+
+
 }
---
+++ src/main/java/SimpleCvsConverter.java
@@ -0,0 +1,12 @@
+public class SimpleCvsConverter {
+
+    public void convert(String file, String outputFormat){
+        System.out.println("I convert CSV to output format");
+    }
+
+    public void convert(String file){
+
+        System.out.println("I convert CSV to output format");
+
+    }
+}
