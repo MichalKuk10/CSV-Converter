2bdd3073ee73d88270d5cdccf32c94cdc4e001b5
michal <kuk.michal3@gmail.com>
2021-01-11 14:18:34 +0100

    add working implementation when no file is provided

---
+++ src/main/java/ConverterApplication.java
@@ -0,0 +1,9 @@
+public class ConverterApplication {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("No input file defined");
+        }
+
+
+    }
+}
